249835234f04b3e28c8040da3503da30a566f5fc
diogenesoftoronto <diogenesoft@protonmail.com>
2021-12-04 23:22:58 -0500

    should have commited a while ago

--- index.js
+++ index.js
@@ -1,13 +1,78 @@
-let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
-    this.amount = amount;
+
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
+    this.transactions = [];
+    }
+  get accountBalance() {
+    // Calculate the balance using the transaction objects.
+    const withdrawls = this.transactions.map(function (transaction) {
+      if (transaction.type === 'withdrawl') {
+        return transaction.amount;}
+     })
+
+    const deposits = this.transactions.map(function (transaction) {
+      if (transaction.type === 'deposit') {
+        return transaction.amount;}
+      })
+
+    if (deposits.length < 0) {
+      this.balance = this.balance - withdrawls.reduce(function (acc, withdrawl) {
+        return acc + withdrawl;
+      });
+    }
+    const withdrawl = withdrawls[Symbol.iterator]();
+
+    const transactionBalances = deposits.map(function (deposit) {
+      return deposit - withdrawl.next().value;})
+
+    this.balance = transactionBalances.reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    });
+    console.log(this.balance);
+    return this.balance;
+  }
+  set addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction extends Account {
+  get commit() {
+    // Keep track of the time of the transaction
+    this.time = new Date();
+    this.addTransaction = this;
   }
 
-  commit() {
-    balance -= this.amount;
+   set transfer (amount) {
+    this.amount = amount;
+
+  }
+
+}
+class Deposit extends Transaction {
+
+  get commit (){
+    super.commit
+    this.balance += this.amount;
+    this.type = "deposit";
+    return this.balance;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get commit() {
+
+    this.balance -= this.amount;
+    super.commit
+    this.type = 'withdrawal';
+    return this.balance;
   }
 
 }
@@ -15,15 +80,22 @@ class Withdrawal {
 
 
 
+
+
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = "snow-patrol";
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+w1 = new Withdrawal(myAccount);
+w1.transfer = 50;
+w1.commit;
+console.log('Transaction 1:', w1.transactions[0]);
+console.log(myAccount)
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+w2 = new Withdrawal(myAccount);
+w2.transfer = 50;
+w2.commit;
+console.log('Transaction 2:', w2.transactions[0]);
 
-console.log('Balance:', balance);
+console.log('Balance:', w2.accountBalance);
+// 
